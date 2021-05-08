c1df1ecaa70cc6960fc489e4aae60c1e53362368
kattaris <kattaris@protonmail.ch>
2021-05-08 17:00:30 +0300

    [+]simple impl
    Signed-off-by: kattaris <kattaris@protonmail.ch>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::{BufRead, Write};
+
+fn main() -> Result<(), crossterm::ErrorKind>{
+    crossterm::terminal::enable_raw_mode()?;
+
+    let input = io::stdin();
+    let output = io::stdout();
+    let mut handle_out = output.lock();
+
+    loop {
+        write!(handle_out, "Type a command >")?;
+        handle_out.flush()?;
+
+        let line = input.lock().lines().next().unwrap()?;
+        if line == "quit" {
+            break;
+        }
+
+        write!(handle_out, "You typed '{}'\n\r", line)?;
+        handle_out.flush()?;
+    }
+
+    crossterm::terminal::disable_raw_mode()?;
+    Ok(())
 }
